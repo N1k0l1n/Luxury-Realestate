00d4c1ab369ed3f2408369fbea3e7e294c583412
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-06-29 12:56:19 +0200

    finnishing up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
+import InfoSection from "./components/InfoSection";
 import Navbar from "./components/Navbar";
 import { sliderData } from "./data/SliderData";
 import GlobalStyle from "./globalStyles";
 import React, { useState } from "react";
+import {InfoData} from "./data/InfoData";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,6 +20,7 @@ function App() {
       <Navbar toggle={toggle}/>
       <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Hero slides={sliderData} />
+      <InfoSection {...InfoData}/>
     </>
   );
 }
---
+++ src/data/InfoData.js
@@ -0,0 +1,25 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/interior-2.jpg';
+
+export const InfoData = {
+  heading: "Explore This Home",
+  paragraphOne:
+    "Discover the finest properties in the most desirable locations.",
+  paragraphTwo:
+    "Explore our portfolio of luxury homes and experience unparalleled elegance.",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Luxury Villa Retreat",
+  paragraphOne: "Indulge in the ultimate luxury villa retreat experience.",
+  paragraphTwo:
+    "Immerse yourself in opulence and serenity with our collection of exquisite villas.",
+  buttonLabel: "Discover More",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
